0ea10ac057673ae223944602f48961696769d4c4
langford <langfordquarshie21@gmail.com>
2023-11-04 19:58:14 +0000

    add semantic html and new ui elements

--- apps/web/app/components/img.tsx
+++ apps/web/app/components/img.tsx
@@ -3,20 +3,20 @@ import * as React from "react";
 type ImageProps = React.ComponentPropsWithoutRef<"img">;
 
 export function Img(props: ImageProps): JSX.Element {
-   return (
-      <img
-         {...props}
-         alt={props.alt}
-         height={props.height}
-         src={props.src}
-         style={{
-            display: "block",
-            outline: "none",
-            border: "none",
-            textDecoration: "none",
-            ...props.style,
-         }}
-         width={props.width}
-      />
-   );
+  return (
+    <img
+      {...props}
+      alt={props.alt}
+      height={props.height}
+      src={props.src}
+      style={{
+        display: "block",
+        outline: "none",
+        border: "none",
+        textDecoration: "none",
+        ...props.style,
+      }}
+      width={props.width}
+    />
+  );
 }
