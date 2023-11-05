7a687da1884d9e2128880ff7015b90cfef67ae29
langford <langfordquarshie21@gmail.com>
2023-11-05 23:17:36 +0000

    update pnpm-lock.yaml

--- apps/web/app/components/draggable.tsx
+++ apps/web/app/components/draggable.tsx
@@ -5,12 +5,12 @@ import React, { useEffect, useState } from "react";
 import { useDraggable } from "@dnd-kit/core";
 import { Resizable } from "re-resizable";
 import {
-   AlignCenter,
-   AlignJustify,
-   AlignLeft,
-   AlignRight,
-   CopyPlus,
-   X,
+  AlignCenter,
+  AlignJustify,
+  AlignLeft,
+  AlignRight,
+  CopyPlus,
+  X,
 } from "lucide-react";
 import { toast } from "sonner";
 import { useAtom } from "jotai";
@@ -19,187 +19,190 @@ import { DRAGGABLE_STYLE } from "../lib/constants";
 import { isGeneratingMarkupAtom } from "../atoms";
 
 export function Draggable(props: {
-   id: string;
-   styles: object;
-   children: ReactNode;
-   onDuplicate: (element: UIElement) => void;
-   onRemove: (element: UIElement) => void;
-   element: UIElement;
+  id: string;
+  styles: object;
+  children: ReactNode;
+  onDuplicate: (element: UIElement) => void;
+  onRemove: (element: UIElement) => void;
+  element: UIElement;
 }): JSX.Element | null {
-   const [isGeneratingMarkup, _] = useAtom(isGeneratingMarkupAtom);
-   const [showToolbar, setShowToolbar] = useState(false);
-   const [currentElement, setCurrentElement] = useState<EventTarget | null>(null);
-   const { attributes, listeners, setNodeRef, transform } = useDraggable({
-      id: props.id,
-   });
+  const [isGeneratingMarkup, _] = useAtom(isGeneratingMarkupAtom);
+  const [showToolbar, setShowToolbar] = useState(false);
+  const [currentElement, setCurrentElement] = useState<EventTarget | null>(
+    null,
+  );
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: props.id,
+  });
 
-   const style = transform
-      ? {
-           transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-        }
-      : undefined;
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
 
-   function toggleShowToolbar(target: EventTarget): void {
-      setShowToolbar(!showToolbar);
-      setCurrentElement(target);
-   }
+  function toggleShowToolbar(target: EventTarget): void {
+    setShowToolbar(!showToolbar);
+    setCurrentElement(target);
+  }
 
-   if (!props.children) return null;
+  if (!props.children) return null;
 
-   return (
-      <Resizable
-         className="resizable"
-         enable={!isGeneratingMarkup ? undefined : false}
-         style={{ ...props.styles, ...style }}
+  return (
+    <Resizable
+      className="resizable"
+      enable={!isGeneratingMarkup ? undefined : false}
+      style={{ ...props.styles, ...style }}
+    >
+      <div
+        className="w-full h-full"
+        onMouseEnter={(e) => {
+          toggleShowToolbar(e.target);
+        }}
+        onMouseLeave={(e) => {
+          toggleShowToolbar(e.target);
+        }}
       >
-         <div
-            className="w-full h-full"
-            onMouseEnter={(e) => {
-               toggleShowToolbar(e.target);
-            }}
-            onMouseLeave={(e) => {
-               toggleShowToolbar(e.target);
-            }}
-         >
-            {showToolbar ? (
-               <DraggableToolBar {...props} currentElement={currentElement} />
-            ) : null}
-            <div
-               ref={setNodeRef}
-               style={{ ...DRAGGABLE_STYLE }}
-               {...listeners}
-               {...attributes}
-               id={props.id}
-            >
-               {props.children}
-            </div>
-         </div>
-      </Resizable>
-   );
+        {showToolbar ? (
+          <DraggableToolBar {...props} currentElement={currentElement} />
+        ) : null}
+        <div
+          ref={setNodeRef}
+          style={{ ...DRAGGABLE_STYLE }}
+          {...listeners}
+          {...attributes}
+          id={props.id}
+        >
+          {props.children}
+        </div>
+      </div>
+    </Resizable>
+  );
 }
 
 function DraggableToolBar(props: {
-   onDuplicate: (element: UIElement) => void;
-   onRemove: (element: UIElement) => void;
-   currentElement: EventTarget | null;
-   element: UIElement;
+  onDuplicate: (element: UIElement) => void;
+  onRemove: (element: UIElement) => void;
+  currentElement: EventTarget | null;
+  element: UIElement;
 }): JSX.Element {
-   const [styles, setStyles] = useState<React.CSSProperties | undefined>({});
+  const [styles, setStyles] = useState<React.CSSProperties | undefined>({});
 
-   const selectedElement = props.currentElement as Element;
-   const selectedElementTarget: HTMLElement | undefined = selectedElement
-      .childNodes[0] as HTMLElement;
+  const selectedElement = props.currentElement as Element;
+  const selectedElementTarget: HTMLElement | undefined = selectedElement
+    .childNodes[0] as HTMLElement;
 
-   useEffect(() => {
-      try {
-         selectedElementTarget.style.backgroundColor = styles?.backgroundColor as string;
-         selectedElementTarget.style.color = styles?.color as string;
-         selectedElementTarget.style.textAlign = styles?.textAlign as string;
-         selectedElementTarget.style.fontSize = styles?.fontSize as string;
-         selectedElementTarget.style.height = styles?.height as string;
-      } catch (error: unknown) {
-         toast.error(String(error));
-      }
-   }, [selectedElementTarget, styles]);
+  useEffect(() => {
+    try {
+      selectedElementTarget.style.backgroundColor =
+        styles?.backgroundColor as string;
+      selectedElementTarget.style.color = styles?.color as string;
+      selectedElementTarget.style.textAlign = styles?.textAlign as string;
+      selectedElementTarget.style.fontSize = styles?.fontSize as string;
+      selectedElementTarget.style.height = styles?.height as string;
+    } catch (error: unknown) {
+      toast.error(String(error));
+    }
+  }, [selectedElementTarget, styles]);
 
-   return (
-      <ul className="absolute -top-4 z-10 bg-white flex items-center border p-1 px-3 space-x-5">
-         <li>
-            <button
-               onClick={() => {
-                  props.onRemove(props.element);
-               }}
-               type="button"
-            >
-               <X />
-            </button>
-         </li>
-         <li>
-            <button
-               onClick={() => {
-                  props.onDuplicate(props.element);
-               }}
-               type="button"
-            >
-               <CopyPlus />
-            </button>
-         </li>
-         {props.element.customizeableStyles?.includes("BG") ? (
-            <li>
-               <input
-                  onChange={(e) => {
-                     setStyles({ ...styles, backgroundColor: e.target.value });
-                  }}
-                  type="color"
-               />
-            </li>
-         ) : null}
-         {props.element.customizeableStyles?.includes("COLOR") ? (
-            <li>
-               <input
-                  onChange={(e) => {
-                     setStyles({ ...styles, color: e.target.value });
-                  }}
-                  type="color"
-               />
-            </li>
-         ) : null}
-         {props.element.customizeableStyles?.includes("TEXT-ALIGN") ? (
-            <li>
-               <div className="flex space-x-5">
-                  <AlignLeft
-                     onClick={() => {
-                        setStyles({ ...styles, textAlign: "left" });
-                     }}
-                  />
-                  <AlignCenter
-                     onClick={() => {
-                        setStyles({ ...styles, textAlign: "center" });
-                     }}
-                  />
-                  <AlignRight
-                     onClick={() => {
-                        setStyles({ ...styles, textAlign: "right" });
-                     }}
-                  />
-                  <AlignJustify
-                     onClick={() => {
-                        setStyles({ ...styles, textAlign: "justify" });
-                     }}
-                  />
-               </div>
-            </li>
-         ) : null}
-         {props.element.customizeableStyles?.includes("FONT-SIZE") ? (
-            <li>
-               <div className="flex gap-1">
-                  <input
-                     className="border w-[50px] text-center"
-                     defaultValue={16}
-                     onChange={(e) => {
-                        setStyles({ ...styles, fontSize: `${e.target.value}px` });
-                     }}
-                     type="number"
-                  />
-                  px
-               </div>
-            </li>
-         ) : null}{" "}
-         {props.element.customizeableStyles?.includes("HEIGHT") ? (
-            <li>
-               <div className="flex gap-1">
-                  <input
-                     className="border w-[50px] text-center"
-                     defaultValue={1}
-                     onChange={(e) => {
-                        setStyles({ ...styles, height: `${e.target.value}px` });
-                     }}
-                     type="number"
-                  />
-                  px
-               </div>
-            </li>
-         ) : null}
-      </ul>
-   );
+  return (
+    <ul className="absolute -top-4 z-10 bg-white flex items-center border p-1 px-3 space-x-5">
+      <li>
+        <button
+          onClick={() => {
+            props.onRemove(props.element);
+          }}
+          type="button"
+        >
+          <X />
+        </button>
+      </li>
+      <li>
+        <button
+          onClick={() => {
+            props.onDuplicate(props.element);
+          }}
+          type="button"
+        >
+          <CopyPlus />
+        </button>
+      </li>
+      {props.element.customizeableStyles?.includes("BG") ? (
+        <li>
+          <input
+            onChange={(e) => {
+              setStyles({ ...styles, backgroundColor: e.target.value });
+            }}
+            type="color"
+          />
+        </li>
+      ) : null}
+      {props.element.customizeableStyles?.includes("COLOR") ? (
+        <li>
+          <input
+            onChange={(e) => {
+              setStyles({ ...styles, color: e.target.value });
+            }}
+            type="color"
+          />
+        </li>
+      ) : null}
+      {props.element.customizeableStyles?.includes("TEXT-ALIGN") ? (
+        <li>
+          <div className="flex space-x-5">
+            <AlignLeft
+              onClick={() => {
+                setStyles({ ...styles, textAlign: "left" });
+              }}
+            />
+            <AlignCenter
+              onClick={() => {
+                setStyles({ ...styles, textAlign: "center" });
+              }}
+            />
+            <AlignRight
+              onClick={() => {
+                setStyles({ ...styles, textAlign: "right" });
+              }}
+            />
+            <AlignJustify
+              onClick={() => {
+                setStyles({ ...styles, textAlign: "justify" });
+              }}
+            />
+          </div>
+        </li>
+      ) : null}
+      {props.element.customizeableStyles?.includes("FONT-SIZE") ? (
+        <li>
+          <div className="flex gap-1">
+            <input
+              className="border w-[50px] text-center"
+              defaultValue={16}
+              onChange={(e) => {
+                setStyles({ ...styles, fontSize: `${e.target.value}px` });
+              }}
+              type="number"
+            />
+            px
+          </div>
+        </li>
+      ) : null}{" "}
+      {props.element.customizeableStyles?.includes("HEIGHT") ? (
+        <li>
+          <div className="flex gap-1">
+            <input
+              className="border w-[50px] text-center"
+              defaultValue={1}
+              onChange={(e) => {
+                setStyles({ ...styles, height: `${e.target.value}px` });
+              }}
+              type="number"
+            />
+            px
+          </div>
+        </li>
+      ) : null}
+    </ul>
+  );
 }
